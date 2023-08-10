bf58a74fdd45ff5122d54171eff558e2e90315f0
Tamira2804 <tamira2804@gmail.com>
2023-08-10 21:03:10 +0300

    Update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactForm from './ContactForm';
+import ContactsList from './ContactsList';
+import Filter from './Filter';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleAddContact = newContact => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  // handleNameChange = event => {
+  //   this.setState({ name: event.target.value });
+  // };
+
+  // formSubmitHandler = data => {
+  //   console.log(data);
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Phonebook</h2>
+        <ContactForm onAddContact={this.handleAddContact} />
+
+        <h2>Contacts</h2>
+        <ContactsList contacts={this.state.contacts} />
+        <Filter onChange={this.changeFilter} />
+      </div>
+    );
+  }
+}
+
+export default App;
