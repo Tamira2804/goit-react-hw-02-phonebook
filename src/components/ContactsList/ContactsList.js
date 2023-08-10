73d3d2977dcfc9457ecc73aad80b64ed0533e94c
Tamira2804 <tamira2804@gmail.com>
2023-08-10 21:03:23 +0300

    contacts

---
+++ src/components/ContactsList/ContactsList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ContactsList = ({ contacts }) => {
+  return (
+    <ul>
+      {contacts.map(contact => (
+        <li key={contact.id}>
+          {contact.name} {contact.number}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContactsList;
